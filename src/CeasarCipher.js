1571cf077c7a9c2d0b76d3dff06412a3f5ea716d
AselEsra <64264345+AselEsra@users.noreply.github.com>
2022-11-07 04:03:33 +0600

    Update CeasarCipher.js

--- src/CeasarCipher.js
+++ src/CeasarCipher.js
@@ -13,7 +13,9 @@ class CaesarCipher extends Component {
   DecryptKey = React.createRef();
 
   handelSubmit = (e) => {
+    // to prevent a full page reload on submit
     e.preventDefault();
+    // check for mode
     if (this.state.mode === 2) {
       const userInput = this.inputTextDecrypt.current.value;
       const results = this.ceaserCipher(
@@ -32,6 +34,7 @@ class CaesarCipher extends Component {
       this.setState({ text: results });
     }
   };
+  // Handel Mode selection click
   handelClick = (opt) => {
     this.setState({ text: "" });
     if (opt === 1) this.setState({ mode: 1 });
@@ -47,18 +50,18 @@ class CaesarCipher extends Component {
           <h5>{this.formatResult()}</h5>
           <hr />
           <button
-            onClick={() => this.handelClick(1)}
-            className="btn"
-            type="button"
-          >
-            Decrypt
-          </button>
-          <button
             onClick={() => this.handelClick(2)}
             className="btn"
             type="button"
           >
             Encrypt
+          </button>
+          <button
+            onClick={() => this.handelClick(1)}
+            className="btn"
+            type="button"
+          >
+            Decrypt
           </button>
           <button className="btn" type="submit">
             RUN
@@ -73,12 +76,12 @@ class CaesarCipher extends Component {
     return mode === 1 ? (
       <div>
         <label htmlfor="inputText" class="visually-hidden">
-          Please Enter the text you want to decrypt:  </label>
+          Please Enter the text you want to encrypt:  </label>
         <textarea
           type="text"
           id="inputText"
           className="form-control"
-          placeholder="Text To Decrypt"
+          placeholder="Text To Encrypt"
           ref={this.inputText}
           autoFocus
           required
@@ -95,13 +98,13 @@ class CaesarCipher extends Component {
     ) : (
       <div>
         <label htmlfor="inputTextDecrypt" class="visually-hidden">
-          Please Enter the text you want to encrypt
+          Please Enter the text you want to decrypt
         </label>
         <textarea
           type="text"
           id="inputTextDecrypt"
           className="form-control"
-          placeholder="Text To Encrypt"
+          placeholder="Text To Decrypt"
           ref={this.inputTextDecrypt}
           required
         ></textarea>
@@ -117,21 +120,22 @@ class CaesarCipher extends Component {
     );
   }
 
+  //decipher the string
   ceaserCipher(text, key, option) {
     let decipher = "";
-    
+    // if option 1 is passed then Encrypt
     if (option === 1) {
-      
+      //decipher each letter
       for (let i = 0; i < text.length; i++) {
-        
+        // check for space
         if (text[i] === " ") decipher += " ";
-        
+        //if letter is uppercase then add uppercase letters
         else if (this.isUpperCase(text[i])) {
           decipher += String.fromCharCode(
             ((text.charCodeAt(i) - key - 39) % 26) + 65
           );
         } else {
-          
+          //else add lowercase letters
           decipher += String.fromCharCode(
             ((text.charCodeAt(i) - key - 71) % 26) + 97
           );
@@ -139,14 +143,18 @@ class CaesarCipher extends Component {
       }
       return decipher;
     } else if (option === 2) {
-     
+      // if option 2 is passed then Decrypt
+      //decipher each letter
       for (let i = 0; i < text.length; i++) {
+        // check for space
         if (text[i] === " ") decipher += " ";
+        //if letter is uppercase then add uppercase letters
         else if (this.isUpperCase(text[i])) {
           decipher += String.fromCharCode(
             ((text.charCodeAt(i) + key - 39) % 26) + 65
           );
         } else {
+          //else add lowercase letters
           decipher += String.fromCharCode(
             ((text.charCodeAt(i) + key - 71) % 26) + 97
           );
@@ -158,6 +166,7 @@ class CaesarCipher extends Component {
     }
   }
 
+  //check if letter is uppercase
   isUpperCase(text) {
     return text === text.toUpperCase();
   }
