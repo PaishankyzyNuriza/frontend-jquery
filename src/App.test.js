a75830773713d9f8c83afcb9a545e98d2be6e589
PaishankyzyNuriza <paishanovanuri@gmail.com>
2023-11-03 05:57:50 +0600

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Please Enter the text you want to encrypt/i);
   expect(linkElement).toBeInTheDocument();
 });
